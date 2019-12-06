3f46540b25a8b1e3bbadcf6acdf3bf1cd7099ac5
wangyang <1140581193@qq.com>
2019-12-06 11:26:20 +0800

    [fengwangyang] 测试git特殊符号

--- src/router.js
+++ src/router.js
@@ -15,9 +15,6 @@ export default new Router({
     {
       path: '/installConfig',
       name: 'install',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "install" */ './views/InstallConfig.vue')
     },
     {
