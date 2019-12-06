4da273303538ed6466b8890615c2bbcf5c2c14fc
wangyang <1140581193@qq.com>
2019-12-06 11:02:11 +0800

    [fengwangyang] 测试时候自动构建

--- src/App.vue
+++ src/App.vue
@@ -57,9 +57,7 @@ html,body{
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  /* text-align: center; */
   color: #2c3e50;
-  // padding-top:60px;
   height:100%;
 }
 </style>
